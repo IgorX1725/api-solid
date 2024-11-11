81ae4a1f114174a6b5b103a0a5a8f824d6e83e15
Igor Cortez <ibarbos2@its.jnj.com>
2024-11-11 14:36:14 -0300

    Creating e2e test for authentication and profile controllers

--- src/http/controllers/tests/profile.spec.ts
+++ src/http/controllers/tests/profile.spec.ts
@@ -2,7 +2,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 import { app } from '@/app'
 
-describe('Authenticate (e2e)', () => {
+describe('Profile (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,21 +11,30 @@ describe('Authenticate (e2e)', () => {
     await app.close()
   })
 
-  it('Should be able to authenticate', async () => {
+  it('Should be able to get user profile', async () => {
     await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@exemple.com',
       password: '123456',
     })
 
-    const response = await request(app.server).post('/sessions').send({
+    const authResponse = await request(app.server).post('/sessions').send({
       email: 'johndoe@exemple.com',
       password: '123456',
     })
 
+    const { token } = authResponse.body
+
+    const response = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
     expect(response.statusCode).toEqual(200)
-    expect(response.body).toEqual({
-      token: expect.any(String),
-    })
+    expect(response.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@exemple.com',
+      }),
+    )
   })
 })
